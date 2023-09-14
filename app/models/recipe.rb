bea278913da3091a02671d494bc2d03d3c3ab1f4
Pierre FILSTROFF <pierre.filstroff@gmail.com>
2023-09-14 21:02:36 +0200

    Add active storage to serve recipes picture (#6)

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
+  has_one_attached :picture
 
   validates :title, presence: true
   validates :ratings, presence: true, numericality: { greater_than: 0 }
--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,7 +1,8 @@
 class RecipeSerializer < ActiveModel::Serializer
   include ActionView::Helpers::DateHelper
+  include Rails.application.routes.url_helpers
 
-  attributes :id, :title, :image, :created, :cooktime, :preptime, :ratings, :category, :author, :ingredients
+  attributes :id, :title, :created, :cooktime, :preptime, :ratings, :category, :author, :picture, :ingredients
 
   def created
     object.created_at.strftime("%B %d, %Y")
@@ -13,6 +14,10 @@ class RecipeSerializer < ActiveModel::Serializer
 
   def preptime
     time_ago_in_words(object.prep_time.minutes.from_now)
+  end
+
+  def picture
+    object.picture.attached? ? url_for(object.picture) : nil
   end
 
   def ingredients
